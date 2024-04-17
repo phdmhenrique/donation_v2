dd3fa711891b49cfd4b63582fcbac1f5e556ee9a
phdmhenrique <pablo.monteirorgt19@gmail.com>
2024-04-17 18:20:50 -0300

    Criação de novos inputs, botções e componentes

--- src/Components/RightSide/RightSide.jsx
+++ src/Components/RightSide/RightSide.jsx
@@ -1,20 +1,30 @@
-import React from 'react'
-import './RightSide.css';
+import React from "react";
+import "./RightSide.css";
 
 function RightSide() {
   return (
-    <div className='rightside'>
+    <div className="rightside">
       <div className="rightside-container">
         <div className="rightside-login">
-
+          <h1>Entrar</h1>
+          <div className="rightside-inputs">
+            <label htmlFor="">Email</label>
+            <input type="text" />
+          </div>
+          <div className="rightside-inputs">
+            <label htmlFor="">Senha</label>
+            <input type="text" />
+          </div>
+          <div className="rightside-buttons">
+            <button className="button">Entrar</button>
+            <span>Esqueceu sua senha?</span>
+          </div>
         </div>
 
-        <div className="righside_other-access">
-
-        </div>
+        <div className="righside_other-access"></div>
       </div>
     </div>
-  )
+  );
 }
 
-export default RightSide
+export default RightSide;
