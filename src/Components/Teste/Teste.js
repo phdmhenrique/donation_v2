c9eec12566532d5502723efe2d4d6fda04249c59
DSM5 <fatec-dsm5@academico.intra>
2024-05-14 22:06:43 -0300

    componente de data com validação feito, faltando apenas seu estilo na página teste.jsx e .js

--- src/Components/Teste/Teste.js
+++ src/Components/Teste/Teste.js
@@ -1,5 +1,27 @@
-import styled from "styled-components";
+import styled from 'styled-components';
+import DatePicker from 'react-datepicker';
+
+export const StyledDatePicker = styled(DatePicker)`
+  position: absolute;
+  top: 40px; /* Ajuste conforme necessário */
+  z-index: 1000; /* Para sobrepor outros elementos */
+`;
 
 export const Container = styled.div`
-    font-size: var(--font__30);
+  display: flex;
+  align-items: center;
+  position: relative;
+`;
+
+export const DateField = styled.div`
+    display: flex;
+    width: 100%;
+    height: 4rem;
+    padding: 0 1rem;
+    border-radius: 0.4rem;
+    border: 0.1rem solid var(--gray-2);
+    background-color: var(--gray-1);
+    transition: 0.2s ease-in-out;
+    align-items: center;
+    cursor: pointer;
 `;
--- src/Components/Teste/Teste.jsx
+++ src/Components/Teste/Teste.jsx
@@ -1,165 +1,68 @@
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { z, ZodError } from "zod";
+import React, { useState, useEffect, useRef } from 'react';
+import { StyledDatePicker, Container, DateField } from './Teste.js';
+import 'react-datepicker/dist/react-datepicker.css';
+import { registerLocale } from 'react-datepicker';
+import ptBR from 'date-fns/locale/pt-BR';
+import { differenceInYears, subYears } from 'date-fns';
 
-import FullSize from "../../Components/FullSize/FullSize.jsx";
-import Divisory from "../../Components/Divisory/Divisory.jsx";
-import LeftSide from "../../Components/LeftSide/LeftSide.jsx";
-import RightSide from "../../Components/RightSide/RightSide.jsx";
-import Footer from "../../Components/Footer/Footer.jsx";
-import Login from "../../Components/RightSide/Login/Login.jsx";
-import Button from "../../Components/Button/Button.jsx";
-import CustomFields from "../CustomFields/CustomFields.jsx";
-import { CustomToastContainer } from "../Notification/Notification.js";
+registerLocale('pt-BR', ptBR);
 
-import imageBanner from "../../Assets/donation-banner.png";
+const Teste = () => {
+  const currentDate = new Date(); // Obtém a data atual
+  const minDateFor18YearsOld = subYears(currentDate, 17); // Calcula a data mínima para ter 18 anos
+  const [selectedDate, setSelectedDate] = useState(minDateFor18YearsOld);
+  const datePickerRef = useRef(null);
 
-function StageOne() {
-  const [formData, setFormData] = useState({
-    cellphone: "",
-    date: "",
-    state: "",
-    city: "",
-    interests: [],
-  });
-
-  const [isButtonEnabled, setIsButtonEnabled] = useState(false);
-  const [activeTab, setActiveTab] = useState(1);
-  const handleBackButton = () => {
-    setActiveTab(1);
-  };
-  const handleChange = (name, value) => {
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    datePickerRef.current.setOpen(false); // Fecha o DatePicker após a seleção
   };
 
-  const fieldsConfigsFirstStep = [
-    {
-      label: "Seu número de telefone",
-      type: "tel",
-      placeholder: "99 99999-9999",
-      name: "cellphone",
-      onChange: handleChange,
-    },
-    {
-      label: "Sua data de nascimento",
-      type: "date",
-      name: "date",
-      onChange: handleChange,
-    },
-    {
-      label: "Estado",
-      type: "select",
-      placeholder: "Selecione um estado",
-      name: "state",
-      onChange: handleChange,
-      options: [
-        { value: "none", label: "Selecionar" },
-        { value: "sp", label: "São Paulo" },
-        { value: "rj", label: "Rio de Janeiro" },
-        { value: "mg", label: "Minas Gerais" },
-        { value: "ba", label: "Bahia" },
-        { value: "pr", label: "Paraná" },
-        { value: "am", label: "Amazonas" },
-      ],
-    },
-    {
-      label: "Cidade",
-      type: "select",
-      placeholder: "Selecione sua cidade",
-      name: "city",
-      onChange: handleChange,
-      options: [
-        { value: "none", label: "Selecionar" },
-        { value: "saopaulo", label: "São Paulo" },
-        { value: "registro", label: "Registro" },
-        { value: "cajati", label: "Cajati" },
-        { value: "jacupiranga", label: "Jacupiranga" },
-        { value: "pariquera-acu", label: "Pariquera-Açu" },
-        { value: "juquia", label: "Juquiá" },
-      ],
-    },
-  ];
+  const handleDateFieldClick = () => {
+    datePickerRef.current.setOpen(true); // Abre o DatePicker
+  };
 
-  const fieldsConfigsSecondStep = [
-    {
-      label: "Grupos de interesse",
-      type: "checkbox",
-      name: "interests",
-      onChange: handleChange,
-      options: [
-        { label: "Esportes", value: "esportes" },
-        { label: "Arte e Cultura", value: "arte-cultura" },
-        { label: "Voluntariado", value: "voluntariado" },
-        { label: "Tecnologia", value: "tecnologia" },
-      ],
-    },
-  ];
+  // Calcula a diferença em anos entre a data atual e a data selecionada
+  const age = differenceInYears(currentDate, selectedDate);
+
+  // Define as cores baseadas na idade
+  const dateFieldColor = age < 18 ? 'red' : 'green';
+
+  // Defina as restrições de data
+  const minDate = subYears(currentDate, 110); // 110 anos atrás a partir da data atual
+  const maxDate = subYears(currentDate, 18); // 18 anos atrás a partir da data atual
 
   return (
-    <FullSize>
-      <Divisory>
-        <LeftSide
-          DonationTitles={["#Cultive", "#Manifeste", "#Impacte"]}
-          customClasses="leftside__more-titles"
-          imgPath={imageBanner}
-          alt="Donation Logo"
-        />
-        <RightSide>
-          <Login
-            pageTitle={
-              <React.Fragment>
-                Prepare-se… <br /> A uma página de distância de usar o DoNation
-              </React.Fragment>
-            }
-            rightsideInputs={
-              activeTab === 1
-                ? fieldsConfigsFirstStep.map((config) => (
-                    <CustomFields key={config.name} {...config} />
-                  ))
-                : fieldsConfigsSecondStep.map((config) => (
-                    <CustomFields key={config.name} {...config} />
-                  ))
-            }
-            formButtons={[
-              <Link
-                to={
-                  activeTab === 1
-                    ? "/create-account"
-                    : "/create-account-stageone"
-                }
-                key="no-key"
-              >
-                <Button
-                  key={1}
-                  addStatusClass="inactive"
-                  onClick={handleBackButton}
-                >
-                  {activeTab === 1 ? "Sair" : "Voltar"}
-                </Button>
-              </Link>,
-              <Button
-                key={2}
-                addStatusClass={isButtonEnabled ? "active" : "disabled"}
-              >
-                {activeTab === 1 ? "Continuar" : "Confirmar"}
-              </Button>,
-            ]}
-            showTabs={true}
-            activeTab={activeTab}
-          />
-
-          <CustomToastContainer
-            toastStyle={{
-              fontSize: "1.4rem",
-            }}
-          />
-        </RightSide>
-      </Divisory>
-      <Footer />
-    </FullSize>
+    <Container>
+      <DateField onClick={handleDateFieldClick} style={{ color: dateFieldColor }}>
+        {selectedDate ? (
+          <>
+            {selectedDate.toLocaleDateString('pt-BR', { month: 'short' })}
+            {' | '}
+            {selectedDate.getDate()}
+            {' | '}
+            {selectedDate.getFullYear()}
+          </>
+        ) : (
+          'Selecione a Data'
+        )}
+      </DateField>
+      <StyledDatePicker
+        ref={datePickerRef}
+        selected={selectedDate}
+        onChange={handleDateChange}
+        dateFormat="dd/MM/yyyy"
+        showMonthDropdown
+        showYearDropdown
+        dropdownMode="select"
+        locale="pt-BR"
+        customInput={<div />} // Usa um div vazio para esconder o input padrão
+        withPortal // Abre o DatePicker em um portal para sobrepor corretamente
+        minDate={minDate}
+        maxDate={maxDate}
+      />
+    </Container>
   );
-}
+};
 
-export default StageOne;
+export default Teste;
